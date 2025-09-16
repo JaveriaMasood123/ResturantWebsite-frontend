094530e52886bc44465fa75c41fdd59b52bb8828
Javeria Masood <javeriamails@gmail.com>
2025-09-16 22:28:06 +0500

    Update auth.js

--- src/services/auth.js
+++ src/services/auth.js
@@ -1,4 +1,4 @@
-const API_URL = "http://localhost:5000/api/auth";
+const API_URL = "https://resturant-website-backend-gamma.vercel.app/api/auth";
 
 export async function login(credentials) {
   const res = await fetch(`${API_URL}/login`, {
